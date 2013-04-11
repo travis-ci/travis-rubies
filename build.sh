ddef980f75d73d938aeb15467dd98baf75cd3cfe
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-04-11 10:21:26 +0200

    initial commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+[[ $RUBY ]] || (echo 'please set $RUBY' && exit 1)
+
+rvm remove $RUBY
+rvm install $RUBY
+rvm prepare $RUBY
+
+gem install travis-artifacts
+travis-artifacts upload --path "$RUBY.tar.bz2"
