40a74c5288d38cd3af07ee6595dc12f88ec34bc1
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-03-24 20:02:55 +0100

    Revert "do not unset CC"
    This reverts commit 30a81def2b3298583c47a8db3b7b065bed9164a8.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ source ./build_info.sh
 [[ $RUBY ]] || { echo 'please set $RUBY' && exit 1; }
 echo "EVERYBODY STAND BACK, WE'RE INSTALLING $RUBY"
 
+unset CC
 if [ `expr $RUBY : '.*-clang$'` -gt 0 ]; then
   export CC=${RUBY##*-}
 fi
