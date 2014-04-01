3328b9a21f363a0c0f3d2d11402bba667707fd8a
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-04-01 08:23:59 -0700

    also build on osx 10.8 branch

--- lib/travis/rubies/update.rb
+++ lib/travis/rubies/update.rb
@@ -10,20 +10,20 @@ module Travis::Rubies
 
     def initialize(options = {})
       @github_token = options[:github_token] || ENV.fetch("GITHUB_TOKEN")
-      @branch       = options[:branch]       || 'build'
+      @branches     = options[:branches]     || ['build', 'osx-10.8']
       @slug         = options[:slug]         || 'travis-ci/travis-rubies'
     end
 
     def build(ruby)
       content = "export RUBY=%s\n" % Shellwords.escape(ruby)
       message = "trigger new build for %s" % ruby
-      write("build_info.sh", content, message)
+      @branches.each { |branch| write("build_info.sh", content, message, branch) }
     end
 
-    def write(path, content, message)
+    def write(path, content, message, branch)
       gh      = GH.with(token: @github_token)
-      payload = { message: message, path: path, content: Base64.strict_encode64(content), branch: @branch }
-      current = gh["repos/#{@slug}/contents/#{path}?ref=#{@branch}"]
+      payload = { message: message, path: path, content: Base64.strict_encode64(content), branch: branch }
+      current = gh["repos/#{@slug}/contents/#{path}?ref=#{branch}"]
       gh.put("repos/#{@slug}/contents/#{path}", payload.merge('sha' => current['sha']))
     rescue GH::Error => error
       raise error unless payload
