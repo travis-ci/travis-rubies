9ba9730c58befe17ffa37119939c184920bca739
Hiro Asari <asari.ruby@gmail.com>
2015-04-13 20:43:42 -0400

    Build 2.2.2 with a special patch

--- build_info.sh
+++ build_info.sh
@@ -1,1 +1,1 @@
-export RUBY=2.1.6
+export RUBY='2.2.2 --patch https://raw.githubusercontent.com/rvm/rvm/2c197d35f533d173ca07491c6367c6cf81d519eb/patches/ruby/2.2.2/fix_installing_bundled_gems.patch'
