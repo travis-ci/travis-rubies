41dbb9de5b73f59933cfaa62e424cdd334e56cef
Hiro Asari <asari.ruby@gmail.com>
2015-05-29 11:35:52 -0400

    Skip checking bundle install for ruby-head
    Fixes https://github.com/travis-ci/travis-ci/issues/3977
    
    I am not sure why the installation fails; given that ruby-head
    if for bleeding edge testing, I *think* we are ok to skip this
    check, so that the tests can continue.

--- build.sh
+++ build.sh
@@ -128,6 +128,9 @@ ruby-1.*)
     announce rvm install $RUBY --verify-downloads 1 --disable-install-doc
   fi;;
 ruby-*)
+  if [[ $RUBY = *head ]]; then
+    announce export SKIP_CHECK=1
+  fi
   if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     curl -L https://get.smf.sh | sh
     rvm autolibs smf
