cacbf45238af28c711a3fea1f2fa80f3abf34b79
Hiro Asari <asari.ruby@gmail.com>
2015-11-14 11:59:16 -0500

    Find which files are linked to compiled ruby

--- build.sh
+++ build.sh
@@ -138,6 +138,8 @@ ruby-1.*)
   else
     announce rvm install $RUBY --verify-downloads 1 --disable-install-doc
   fi;;
+
+  rvm $RUBY do ldd `which ruby`
 ruby-*)
   if [[ $RUBY = *head ]] || [[ $RUBY = *head-clang ]]; then
     announce export SKIP_CHECK=1
