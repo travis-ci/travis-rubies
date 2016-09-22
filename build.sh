414d1c08641a7301e23fc85df5c2753f52cbba72
Hiro Asari <asari.ruby@gmail.com>
2016-09-22 18:55:12 -0400

    Revert "Build ruby-head with RVM head"
    This reverts commit f02c8b69098b7b725149f9fe2c955a5c67f94b29.

--- build.sh
+++ build.sh
@@ -56,8 +56,7 @@ function update_mvn() {
 # update rvm
 fold_start rvm.1 "update rvm"
 announce rvm remove 1.8.7
-rvm_reload_flag=1 source '/Users/travis/.rvm/scripts/rvm'
-announce rvm get head
+announce rvm get stable
 announce rvm reload
 announce rvm cleanup all
 fold_end rvm.1
