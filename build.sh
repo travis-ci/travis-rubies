1067dadecbae7194097146e59f5214cfb7196025
Hiro Asari <asari.ruby@gmail.com>
2016-09-27 20:01:10 -0400

    Add timing information for each build step

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 announce() {
+  travis_time_start
   echo \$ $@
   $@
+  travis_time_finish
 }
 
 travis_retry() {
@@ -50,6 +52,34 @@ function update_mvn() {
   export M2_HOME=$PWD/apache-maven-$VERSION
   mvn -version
   fold_end mvn.1
+}
+
+travis_time_start() {
+  travis_timer_id=$(printf %08x $(( RANDOM * RANDOM )))
+  travis_start_time=$(travis_nanoseconds)
+  echo -en "travis_time:start:$travis_timer_id\r${ANSI_CLEAR}"
+}
+
+travis_time_finish() {
+  local result=$?
+  travis_end_time=$(travis_nanoseconds)
+  local duration=$(($travis_end_time-$travis_start_time))
+  echo -en "\ntravis_time:end:$travis_timer_id:start=$travis_start_time,finish=$travis_end_time,duration=$duration\r${ANSI_CLEAR}"
+  return $result
+}
+
+function travis_nanoseconds() {
+  local cmd="date"
+  local format="+%s%N"
+  local os=$(uname)
+
+  if hash gdate > /dev/null 2>&1; then
+    cmd="gdate" # use gdate if available
+  elif [[ "$os" = Darwin ]]; then
+    format="+%s000000000" # fallback to second precision on darwin (does not support %N)
+  fi
+
+  $cmd -u $format
 }
 
 #######################################################
