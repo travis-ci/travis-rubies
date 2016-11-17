e49b36340bc0cb0e8f737072dcabee716ac5a329
Hiro Asari <asari.ruby@gmail.com>
2016-11-16 19:51:00 -0500

    Remove preprocessing for webhook receiver
    Don't do any unnecessary work

--- lib/travis/rubies/web/hook.rb
+++ lib/travis/rubies/web/hook.rb
@@ -2,13 +2,6 @@ require 'json'
 
 module Travis::Rubies::Web
   class Hook < Sinatra::Base
-    set :signatures, ENV.fetch('TRAVIS_SIGNATURES').split(':')
-
-    before do
-      request.body.rewind
-      @payload = JSON.parse(params[:payload])
-    end
-
     post '/:ruby' do
       "OK"
     end
